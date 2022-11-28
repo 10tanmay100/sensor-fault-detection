aeb8a52bfe9007b57b1fb7ec8d4ca837f65922ab
tanmay234 <chakrabortytanmay326@gmail.com>
2022-11-28 09:34:05 +0530

    setup py file created

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # sensor-fault-detection
 The Air Pressure System (APS) is a critical component of a heavy-duty vehicle that uses compressed air to force a piston to provide pressure to the brake pads, slowing the vehicle down. The benefits of using an APS instead of a hydraulic system are the easy availability and long-term sustainability of natural air.
+# Steps
+1. Create setup.py file(We create it to make package)
+2. Created sensor package for this project(
+    creating a folder and inside that create __init__.py
+)
+3. Group of libraries called framework
---
+++ setup.py
@@ -0,0 +1,30 @@
+#use to create a proper package for this project.
+from setuptools import find_packages,setup
+from typing import List
+#defining the function reponsible for install requirement file
+def get_requirement_file()->List[str]:
+    '''
+        Responsible to read all library names from txt file
+    '''
+    with open("requirements.txt","r") as f:
+        return f.readlines()
+
+
+
+
+setup(
+    name="sensor",
+    version="0.0.2",
+    author="Tanmay",
+    author_email="chakrabortytanmay326@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirement_file(),
+)
+
+
+
+
+
+
+
+
