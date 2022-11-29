7f130a8f3b9b07882138b00c6122b5aff0c4a922
10tanmay100 <chakrabortytanmay744@gmail.com>
2022-11-29 21:05:55 +0530

    database constant code added

--- Sensor/constant/database.py
+++ Sensor/constant/database.py
@@ -1,4 +1,4 @@
 import urllib
 DATABASE_NAME="ineuron"
 PASSWORD=""
-MONGODB_URL=f"mongodb+srv://Tanmay:{urllib.parse.quote(PASSWORD)}@industryproject.pgjgvlc.mongodb.net/?retryWrites=true&w=majority&authSource=admin"
+MONGODB_URL=f""
