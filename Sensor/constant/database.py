cde304814da05cafbc43a2b924ab4625762eb0e5
Tanmay Chakraborty <72690313+10tanmay100@users.noreply.github.com>
2022-11-29 21:03:41 +0530

    Delete database.py

--- Sensor/constant/database.py
+++
@@ -1,4 +0,0 @@
-import urllib
-DATABASE_NAME="ineuron"
-PASSWORD=""
-MONGODB_URL=f"mongodb+srv://Tanmay:{urllib.parse.quote(PASSWORD)}@industryproject.pgjgvlc.mongodb.net/?retryWrites=true&w=majority&authSource=admin"
